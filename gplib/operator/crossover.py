9b6f7c4059676c44312e0a3517074ceecad312a3
Hironori Yamamoto <h.yamamoto@ss.cs.osakafu-u.ac.jp>
2018-04-22 22:09:24 +0900

    Added some TODO and change some function names in crossover.py

--- gplib/operator/crossover.py
+++ gplib/operator/crossover.py
@@ -15,31 +15,35 @@ class AbstractCrossover(object):
         :param c_rate: float([0, 1.0]). crossover rate for each iteration
         :param destructive: bool. if it is destructive operator or not(keep parents or not)
         """
+        # TODO check c_rate in range (0, 1]
         self.c_rate = c_rate
         self.destructive = destructive
 
     def __call__(self, parents):
         raise NotImplementedError
 
-    def __get_crossover_point__(self, parent, k=1):
+    @staticmethod
+    def _get_crossover_point(parent, k=1):
+        # TODO check k <= len(nodelist) or get min(k, len(nodelist))
         node_list = node.get_all_node(parent.root)
         points = random.sample(node_list, k=k)
         return points
 
-    def __crossover__(self, parents, points):
+    def _crossover(self, parents, points_set):
         """
         function of applying crossover
 
         :param parents: list of Node objects. list of root node of parents
-        :param points: list of Node objects. list of replaced node
+        :param points_set: list of Node objects. list of replaced node
         :return: root node of new solution.
         """
 
+        # TODO check len(parents) == 2
         if not self.destructive:
             parents = copy.deepcopy(parents)
 
         if random.random() < self.c_rate:
-            for point1, point2 in zip(points[0], points[1]):
+            for point1, point2 in zip(points_set[0], points_set[1]):
                 if parents[0].root is point1:
                     parents[0].root = point2
                 else:
@@ -61,10 +65,11 @@ class OnePointCrossover(AbstractCrossover):
         super().__init__(c_rate, destructive)
 
     def __call__(self, parents):
-        points = []
+        points_set = []
         for parent in parents:
-            points.append(self.__get_crossover_point__(parent))
-        return self.__crossover__(parents, points)
+            points_set.append(self._get_crossover_point(parent))
+        return self._crossover(parents, points_set)
+
 
 def get_default_crossover():
 
