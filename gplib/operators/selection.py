475e811bfb7a018c175de0a3e6bf46c5bd5ccaa5
yossuii <tanaka@ss.cs.osakafu-uac.jp>
2018-08-20 21:27:06 +0900

    update selection

--- gplib/operators/selection.py
+++ gplib/operators/selection.py
@@ -4,21 +4,11 @@ from gplib.solutions.solution import is_solution_in_pop, copy_solution
 
 class AbstractSelection(object):
 
-    def __init__(self, k, replacement, problem):
+    def __init__(self, problem):
         """
-        :param k: int. The number of solutions which are selected.
-        :param replacement: bool. sample with replacement
         :return: function of selection
         """
-        self.k = k
-
-        if replacement:
-            self.rand_f = random.choices
-        else:
-            self.rand_f = random.sample
-
         self.problem = problem
-        self.replacement = replacement
 
     def __call__(self, *args, **kwargs):
         raise NotImplementedError
@@ -30,8 +20,8 @@ class AbstractSelection(object):
 
 class RandomSelection(AbstractSelection):
 
-    def __init__(self, k, replacement):
-        super().__init__(k, replacement, None)
+    def __init__(self):
+        super().__init__(None)
 
     def __call__(self, population):
         """Random Selection
@@ -40,56 +30,59 @@ class RandomSelection(AbstractSelection):
                 population: list of individual. a candidate set of solutions.
 
             # Returns
-                list of solutions.
+                solution.
         """
-        if self.replacement:
-            chosen = []
-            for i in range(self.k):
-                copy_append(random.choice(population), chosen)
-            return chosen
-        else:
-            return self.rand_f(population=population, k=self.k)
+        return random.choice(population=population)
 
 
-class TournamentSelection(AbstractSelection):
-    def __init__(self, tournament_size, problem, replacement=True, selection_size=None):
-        if not replacement and selection_size is None:
-            msg = 'If replacement is False, selection_size must be set.'
-            raise TypeError(msg)
-        self.selection_size = selection_size
-        self.tournament_size = tournament_size
-        if replacement:
-            def append(solution, chosen):
-                return copy_append(solution, chosen)
-        else:
-            def append(solution, chosen):
-                if not is_solution_in_pop(solution, chosen, as_tree=True):
-                    chosen.append(solution)
-
-        self.append = append
-        self.replacement = replacement
-        super().__init__(k=tournament_size, replacement=False, problem=problem)
+class EliteSelection(AbstractSelection):
+    def __init__(self, problem):
+        super().__init__(problem=problem)
 
     def __call__(self, population):
         """
         Tournament Selection
 
         :param population: list of solutions.
-        :return: list of solutions.
+        :return: solution.
         """
-
-        chosen = []
         self._cal_fitness(population)
-        #TODO: we must check the original population size > k (it is also super redundant if k is almost equal to population size), if replacement=False.
-        selection_size = self.selection_size or len(population)
-        while len(chosen) < selection_size:
-            candidates = self.rand_f(population=population, k=self.tournament_size)
-            best = max(candidates, key=lambda x: x.previous_fitness)
-            self.append(best, chosen)
+        return max(population, key=lambda x: x.previous_fitness)
 
-        del population
 
+class SelectionBase(object):
+    def __init__(self, selection_size, replacement, selection_type=None):
+        """
+            Selection Base
+
+            :param selection_size: number of selection solutions.
+            :param selection_type: string. type of selection.
+            :param replacement: bool. sample with replacement
+            :return: list of solutions.
+        """
+        self.selection_size = selection_size
+        self.selection_type = selection_type
+        self.replacement = replacement
+
+    def __call__(self, population):
+        selection_core = self.get_selection()
+        chosen = []
+        if self.replacement:
+            for i in range(self.selection_size):
+                copy_append(selection_core(population), chosen)
+        else:
+            candidates = population[:]
+            for i in range(self.selection_size):
+                solution = selection_core(candidates)
+                chosen.append(solution)
+                candidates.remove(solution)
         return chosen
+
+    def get_selection(self):
+        if self.selection_type == 'elite':
+            return EliteSelection
+        else:
+            return RandomSelection
 
 
 def reduce_population(population):
