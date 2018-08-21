0cf99436ed1409200a1c397b6fa2079198b7b7a1
Hironori Yamamoto <h.yamamoto@ss.cs.osakafu-u.ac.jp>
2018-08-22 01:04:59 +0900

    Develop a test for `get_generator_builder`

---
+++ tests/utils/util.py
@@ -0,0 +1,29 @@
+import unittest
+
+from gplib.operators import RandomSelection
+from gplib.solutions.node import Node
+from gplib.solutions.solution import Solution
+from gplib.utils.util import get_generator_builder
+
+
+class TestUtil(unittest.TestCase):
+    def test_get_generator_builder(self):
+        n_pop = 100
+        population = [Solution(Node())] * n_pop
+        k = 10
+        selection = RandomSelection(k=k, replacement=True)
+
+        generator_builder = get_generator_builder(selection)
+        generator = generator_builder(population)
+
+        n_selected = 10
+        selected_pop_list = []
+        for pop in generator:
+            selected_pop_list.append(pop)
+            if len(selected_pop_list) == n_selected:
+                break
+
+        self.assertEqual(len(selected_pop_list), n_selected)
+
+        for pop in selected_pop_list:
+            self.assertEqual(len(pop), k)
