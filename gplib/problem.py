480633fea44db9e0e116f4939d01363d391cb8d5
Taku Hasegawa <epii.hstko@gmail.com>
2018-08-22 17:52:23 +0900

    implement a base of MLPS-GP and mlps_crossover.

--- gplib/problem.py
+++ gplib/problem.py
@@ -9,14 +9,21 @@ class AbstractProblem(object):
 
     def fitness(self, target_solution_or_solutions):
         if isinstance(target_solution_or_solutions, Solution):
-            self._cal_fitness(target_solution=target_solution_or_solutions)
+            fitness = self._cal_fitness(target_solution=target_solution_or_solutions)
             self._eval_cnt += 1
+            target_solution_or_solutions.previous_fitness = fitness
+            return fitness
         elif isinstance(target_solution_or_solutions, list):
+            fitness_list = []
             for s in target_solution_or_solutions:
-                self._cal_fitness(target_solution=s)
+                fitness = self._cal_fitness(target_solution=s)
+                s.previous_fitness = fitness
+                fitness_list.append(fitness_list)
             self._eval_cnt += len(target_solution_or_solutions)
+            return fitness_list
         else:
             raise TypeError("target_solution_or_solutions should be Solution object or list of Solution objects.")
+
 
     def _cal_fitness(self, target_solution):
         raise NotImplementedError("_cal_fitness should be implemented in {}".format(self.__class__))
