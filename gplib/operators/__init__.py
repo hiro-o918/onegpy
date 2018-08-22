6e6eaef0c814a2efac96e093b0712b547d61dd51
Hironori Yamamoto <h.yamamoto@ss.cs.osakafu-u.ac.jp>
2018-08-22 16:31:16 +0900

    Update __init__.py

--- gplib/operators/__init__.py
+++ gplib/operators/__init__.py
@@ -1,4 +1,13 @@
-from gplib.operators.selection import AbstractSelection
-from gplib.operators.selection import RandomSelection
-from gplib.operators.selection import TournamentSelection
-from gplib.operators.selection import reduce_population
+# Import crossovers
+import gplib.operators.crossover
+# Import initializing functions
+import gplib.operators.initializer
+# Import mutations
+import gplib.operators.mutation
+# import selections
+import gplib.operators.selection
+from gplib.operators.crossover import OnePointCrossover, PopulationOnePointCrossover
+from gplib.operators.initializer import initialize, population_initialize
+from gplib.operators.mutation import PointMutation, PopulationPointMutation
+from gplib.operators.selection import AbstractSelection, RandomSelection, TournamentSelection, EliteSelection, \
+    reduce_population
