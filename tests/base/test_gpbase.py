1cb93b6e2cd7e5a69b3697bf38e5aaea13db5ec8
Hironori Yamamoto <h.yamamoto@ss.cs.osakafu-u.ac.jp>
2018-08-28 02:48:07 +0900

    Update test cases

--- tests/base/test_gpbase.py
+++ tests/base/test_gpbase.py
@@ -1,43 +1,32 @@
 import unittest
 
 from gplib.operator import AbstractOperator
+from gplib.operators import PopulationRandomInitializer, TournamentSelection
 from gplib.operators.crossover import RandomSelection, PopulationOnePointCrossover, OnePointCrossover
+from gplib.problem import AbstractProblem
 from gplib.sequential import Sequential
 from gplib.solutions.node import Node
 from gplib.solutions.solution import Solution
 from gplib.base import gpbase
 from gplib.viewer import viewer
 from gplib.solutions import node
-
-
-def f_non_terminal(n_children=2):
-    def print_non_terminal(x):
-        print('non_terminal')
-
-    return node.build_func(print_non_terminal, n_children)
-
-
-def f_terminal(n_children=2):
-    def print_terminal(x):
-        print('terminal')
-
-    return node.build_func(print_terminal, n_children)
+from tests.test_problem import EmptyProblem
 
 
 class TestSequential(unittest.TestCase):
     def setUp(self):
-        self.sequential = Sequential([PopulationOnePointCrossover(c_rate=1)])
-
-        self.func_dicts = ({0: f_non_terminal()}, {0: f_terminal()})
-
+        self.problem = EmptyProblem()
+        self.sequential = Sequential([PopulationOnePointCrossover(c_rate=1),
+                                      TournamentSelection(k=10, tournament_size=3, problem=self.problem)])
+        self.initializer = PopulationRandomInitializer(k=10, t_prob=0.5, max_depth=3, problem=self.problem)
 
     def test_gp(self):
         # self.sequential.add(self.sl)
         # self.sequential.add(self.co)
 
-        gp = gpbase.PopulationGP(sequential=self.sequential, n_generations=10, viewer=viewer.DefaultViewer())
-
-        gp.__call__(0.9, 3, 50, self.func_dicts)
+        gp = gpbase.PopulationGP(initializer=self.initializer, sequential=self.sequential,
+                                 n_generations=10, viewer=viewer.DefaultViewer())
+        gp.__call__()
 
 
 if __name__ == '__main__':
