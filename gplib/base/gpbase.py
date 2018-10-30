3837ae92e48517b70caae8e0c05188315543ba90
Taku Hasegawa <epii.hstko@gmail.com>
2018-10-30 19:31:34 +0900

    add comment in operator.py and *.py in operator package.

--- gplib/base/gpbase.py
+++ gplib/base/gpbase.py
@@ -7,20 +7,33 @@ Copyright (c) 2018, Taku Hasegawa.
 License: MIT (see LICENSE for details)
 """
 
-__author__ = 'Taku Hasegawa'
-__version__ = '0.0.1-alpha'
+__author__ = 'Taku Hasegawa, '
+__version__ = '0.1.0-beta'
 __license__ = 'MIT'
 
 
 class PopulationGP(object):
-
+    """
+    base class of population based GP
+    """
     def __init__(self, n_generations, initializer, sequential,  viewer):
+        """
+
+        :param n_generations: int. the number of generations
+        :param initializer: population initializer object. initializer of population
+        :param sequential: sequential object. genetic operators
+        :param viewer: viewer object. viewer for population based GP
+        """
         self.n_generations = n_generations
         self.initializer = initializer
         self.sequential = sequential
         self.viewer = viewer
 
     def __call__(self):
+        """
+        optimize a target problem.
+        :return:
+        """
         population = self.initializer()
         self.viewer.begin()
         for gene in range(self.n_generations):
