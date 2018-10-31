59157159b1273f98ac688c5fd86ca71a5122a1f2
Hironori Yamamoto <h.yamamoto@ss.cs.osakafu-u.ac.jp>
2018-10-31 20:05:17 +0900

    Fix tests

--- tests/base/test_gpbase.py
+++ tests/base/test_gpbase.py
@@ -4,6 +4,8 @@ from gplib.operators import PopulationRandomInitializer, TournamentSelection
 from gplib.operators.crossover import PopulationOnePointCrossover
 from gplib.sequential import Sequential
 from gplib.base import gpbase
+from gplib.terminal_condition import TerminalCondition
+from gplib.terminator import GenerationTerminator
 from gplib.viewers import observer
 from tests.test_problem import EmptyProblem
 
@@ -15,12 +17,12 @@ class TestSequential(unittest.TestCase):
                                       TournamentSelection(k=10, tournament_size=3, problem=self.problem)])
         self.initializer = PopulationRandomInitializer(k=10, t_prob=0.5, max_depth=3, problem=self.problem)
 
-    def test_gp(self):
-        # self.sequential.add(self.sl)
-        # self.sequential.add(self.co)
+        self.terminal_condition = TerminalCondition([GenerationTerminator(10)])
 
+    def test_gp(self):
         gp = gpbase.PopulationGP(initializer=self.initializer, sequential=self.sequential,
-                                 n_generations=10, observer=observer.DefaultObserver(verbose=3))
+                                 n_generations=10, terminal_condition=self.terminal_condition,
+                                 observer=observer.DefaultObserver(verbose=3))
         gp.__call__()
 
 
--- tests/viewers/test_loggers.py
+++ tests/viewers/test_loggers.py
@@ -1,23 +1,13 @@
 import unittest
 
-from gplib.viewers.loggers import PrintLogger
+from gplib.viewers.loggers import PrintLogger, JSONLogger
 from gplib.solutions import solution, node
 
 
 class TestPrintLogger(unittest.TestCase):
     def setUp(self):
         self.logger = PrintLogger()
-        s1 = solution.Solution(node.Node())
-        s2 = solution.Solution(node.Node())
-        s3 = solution.Solution(node.Node())
-        solution.set_previous_fitness(s1, 1)
-        solution.set_previous_fitness(s2, 2)
-        solution.set_previous_fitness(s3, 3)
 
-        self.pop = []
-        for _ in range(2):
-            self.pop.extend([s1, s2, s3])
-        self.pop = self.pop*2
 
     def test_begin(self):
         self.logger.begin()
@@ -30,8 +20,17 @@ class TestPrintLogger(unittest.TestCase):
 
 
 class TestJSONLogger(unittest.TestCase):
-    # TODO: write this
-    pass
+    def setUp(self):
+        self.logger = JSONLogger()
+
+    def test_begin(self):
+        self.logger.begin()
+
+    def test_update(self):
+        self.logger.update(1)
+
+    def test_end(self):
+        self.logger.end('log')
 
 
 if __name__ == '__main__':
