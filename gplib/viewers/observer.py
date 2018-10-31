52f9f90fa32349d6839a2155c30ac22ba567a919
Hironori Yamamoto <h.yamamoto@ss.cs.osakafu-u.ac.jp>
2018-10-31 18:47:12 +0900

    Develop MLPS_Observer

--- gplib/viewers/observer.py
+++ gplib/viewers/observer.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from operator import itemgetter
+
+import numpy as np
 
 from gplib.utils import util
 from gplib.viewers.loggers import JSONLogger, PrintLogger
@@ -88,10 +91,64 @@ class DefaultObserver(AbstractObserver):
         [l.end(log=log, history=self.history) for l in self._loggers]
 
     def update(self, gene, population):
-        general_log = {'generation': gene, 'pop_num': len(population)}
-        fit_log = util.get_fitness_info(population)
-        log = {**general_log, **fit_log}
+        log = generate_sgp_log(gene, population)
         self.history.append(log)
 
         [l.update(log=log, history=self.history) for l in self._loggers]
 
+
+class MLPS_Observer(AbstractObserver):
+    def __init__(self, verbose=3, **kwargs):
+        """
+        Observer for MLPS-GP.
+
+            :param verbose: int.
+            0 -> No log,
+            1 -> print a log.
+            2 -> save a log.
+            3 -> print and sve a log
+            :param out: String.output output to print.
+            :param filedir: filedir: str or Path. Path to dump json.
+            :param save_every_updating: bool. if true, save history every updating.
+             otherwise save only in the end.
+        """
+
+        super(MLPS_Observer, self).__init__()
+        self.history_tag = ['detail', 'summary']
+        self.history_dict = {t: [] for t in self.history_tag}
+        self.loggers_mapper = {self.history_tag[0]: [1],  # detail
+                               self.history_tag[1]: [0, 2]}  # summary
+        self.set_loggers(verbose, **kwargs)
+
+    def set_loggers(self, verbose, **kwargs):
+        if verbose & 0b01:
+            self._loggers.append(PrintLogger(**kwargs))
+        if verbose & 0b10:
+            self._loggers.extend([JSONLogger(filename='{}_log'.format(t), **kwargs)
+                                  for t in self.history_tag])
+
+    def begin(self):
+        [l.begin() for l in self._loggers]
+
+    def end(self, iter_cnt, eval_cnt, population_list):
+        log_dict = generate_mlpsgp_log(iter_cnt, eval_cnt, population_list)
+        for t in self.history_tag:
+            self.history_dict[t].append(log_dict[t])
+            if len(self.loggers_mapper[t]) == 1:
+                l = itemgetter(*self.loggers_mapper[t])(self._loggers)
+                l.end(log=log_dict[t], history=self.history_dict[t])
+            else:
+                [l.end(log=log_dict[t], history=self.history_dict[t])
+                 for l in itemgetter(*self.loggers_mapper[t])(self._loggers)]
+
+    def update(self, iter_cnt, eval_cnt, population_list):
+        log_dict = generate_mlpsgp_log(iter_cnt, eval_cnt, population_list)
+
+        for t in self.history_tag:
+            self.history_dict[t].append(log_dict[t])
+            if len(self.loggers_mapper[t]) == 1:
+                l = itemgetter(*self.loggers_mapper[t])(self._loggers)
+                l.update(log=log_dict[t], history=self.history_dict[t])
+            else:
+                [l.update(log=log_dict[t], history=self.history_dict[t])
+                 for l in itemgetter(*self.loggers_mapper[t])(self._loggers)]
