4f81ad3dbc794df9060dabf5caff6b03e288be16
Hironori Yamamoto <h.yamamoto@ss.cs.osakafu-u.ac.jp>
2018-11-01 18:23:43 +0900

    Add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='ONEG',
+    version='0.1.0b',
+    install_requires=['numpy'],
+    extras_require={
+        'tests': ['pytest'],
+        'develop': ['restructuredtext-lint>=1.1.3',
+                    'sphinx-rtd-theme>=0.4.1'
+                    'sphinx>=1.8.1',
+                    'sphinx-autobuild>=0.7.1',
+                    'recommonmark>=0.4.0',
+                    'm2r>=0.2']
+    },
+    license='MIT License'
+)
