e248d8710f1a6e0872844a171e9f33e5de8712b6
Hironori Yamamoto <h.yamamoto@ss.cs.osakafu-u.ac.jp>
2018-11-06 16:13:16 +0900

    Integrate terminators

--- onegpy/terminator.py
+++ onegpy/terminator.py
@@ -5,6 +5,7 @@ class AbstractTerminator(ABC):
     """
     Base of Terminator class
     """
+
     def __call__(self):
         return True
 
@@ -87,3 +88,37 @@ def terminator_checker(terminator):
         return
 
     raise typ(msg)
+
+
+class TerminalCondition(object):
+    def __init__(self, terminators=None):
+        """
+        This class handles a set of terminators.
+        :param terminators: list of Terminator
+        """
+        if terminators is None:
+            terminators = []
+        self._terminators = []
+        for terminator in terminators:
+            self.add(terminator)
+
+    def add(self, terminator):
+        """
+        Add a terminator to an instance of condition.
+        :param terminator: Terminator
+        """
+        terminator_checker(terminator)
+        self._terminators.append(terminator)
+
+    @property
+    def terminators(self):
+        return self._terminators
+
+    def __call__(self):
+        """
+        If any one of terminators indicates `True`, return `True`.
+        Otherwise returns False
+        :return is_t_condition: bool.
+        """
+        is_t_condition = any([t() for t in self._terminators])
+        return is_t_condition
