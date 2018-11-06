6798111cbe6cd081f056d11ae63b5de5fbc3deb2
Hironori Yamamoto <h.yamamoto@ss.cs.osakafu-u.ac.jp>
2018-11-06 16:27:18 +0900

    Integrate terminator

--- tests/test_terminator.py
+++ tests/test_terminator.py
@@ -3,6 +3,9 @@
 import unittest
 
 from onegpy.terminator import GenerationTerminator
+from onegpy.terminator import TerminalCondition
+from onegpy.terminator import AbstractTerminator
+from onegpy.problems import arithmetic
 
 
 class TestGenerationTerminator(unittest.TestCase):
@@ -15,6 +18,31 @@ class TestGenerationTerminator(unittest.TestCase):
         while not self.t1():
             gene += 1
         self.assertEqual(gene, self.t_gene)
+
+
+class TestTrue(AbstractTerminator):
+    def __call__(self):
+        return True
+
+
+class TestFalse(AbstractTerminator):
+    def __call__(self):
+        return False
+
+
+class TestTerminalCondition(unittest.TestCase):
+    def setUp(self):
+        self.problem = arithmetic.Cos2XProblem(1)
+        self.t1 = TestFalse()
+        self.t2 = TestTrue()
+        self.T1 = TerminalCondition([self.t1, self.t1])
+        self.T2 = TerminalCondition([self.t2, self.t2])
+
+    def testT1(self):
+        self.assertFalse(self.T1())
+
+    def testT2(self):
+        self.assertTrue(self.T2())
 
 
 if __name__ == '__main__':
